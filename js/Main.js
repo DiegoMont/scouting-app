f723d477f82cfb80bce5a842743e24655f8de391
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-11 13:53:51 -0600

    Display questions with JS

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,10 @@
 const router = new Router();
+const matchForm = new MatchScoutingForm('#scouting-match form');
+const pitForm = new PitScoutingForm('#scouting-pit form');
 
 addListenersToBtns();
+createMatchForm();
+createPitForm();
 router.openMenu();
 
 function addListenersToBtns() {
@@ -19,4 +23,13 @@ function addListenersToBtns() {
     document.getElementById('to-results-btn').addEventListener('click', function() {
         router.openResults();
     });
+}
+
+function createMatchForm() {
+    matchForm.sections.autonomous.addQuestion(new RadioWithImages('Ojito'));
+    matchForm.renderSections();
+}
+
+function createPitForm() {
+    
 }
