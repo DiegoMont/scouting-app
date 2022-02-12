1011a982a75fabc1e98a79b34f279868d46b35ef
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-12 00:26:45 -0600

    Image buttons classes and styles

--- js/Questions.js
+++ js/Questions.js
@@ -1,8 +1,42 @@
+class NumericText extends Question {
+    constructor(question, name, error, placeholder, minValue, maxValue){
+        super('', name, error);
+        this.questionContainer = document.createElement('div');
+        this.inputs[0] = this.getInput(placeholder);
+        const label = this.getLabel(question);
+        this.questionContainer.appendChild(label);
+        this.questionContainer.appendChild(this.inputs[0]);
+    }
+
+    addToContainer(container){
+        container.appendChild(this.questionContainer);
+        container.appendChild(this.error);
+    }
+
+    getLabel(labelTxt){
+        const label = document.createElement('label');
+        label.for = this.name;
+        label.innerText = labelTxt;
+        return label;
+    }
+
+    getInput(placeholder){
+        const numericInput = document.createElement('input');
+        numericInput.type = 'number';
+        numericInput.name = this.name;
+        numericInput.id = this.name;
+        numericInput.placeholder = placeholder;
+        numericInput.inputMode = 'numeric';
+        return numericInput;
+    }
+}
+
+
 class RadioWithImages extends Question {
     constructor(question, name, error='Debes seleccionar una opción'){
         super(question, name, error);
         this.questionContainer = document.createElement('div');
-        this.questionContainer.classList.add('radio-imagenes', 'flexbox');
+        this.questionContainer.classList.add('image-label-container', 'flexbox');
     }
 
     addInput(inputData){
@@ -15,7 +49,6 @@ class RadioWithImages extends Question {
 
     getInput(inputData){
         const input = document.createElement('input');
-        input.classList.add(ocultar);
         input.type = 'radio';
         input.name = this.name;
         input.id = inputData.id;
@@ -24,15 +57,16 @@ class RadioWithImages extends Question {
     }
 
     getLabel(labelData){
-        const label = document.createElement(label);
-        label.classList.add('img-opc', 'btn');
-        label.for = 'labelData.';
+        const label = document.createElement('label');
+        label.classList.add('btn');
+        label.htmlFor = labelData.id;
         const img = document.createElement('img');
         img.src = labelData.img;
         label.appendChild(img);
         return label;
     }
 }
+
 
 class RegionalSelector extends Question {
 
@@ -71,5 +105,41 @@ class RegionalSelector extends Question {
             select.appendChild(regionalOption);
         }
         return select;
+    }
+}
+
+
+class CheckboxWithImages extends RadioWithImages {
+    constructor(question, name, error='Debes seleccionar al menos una opción'){
+        super(question, name, error);
+    }
+
+    getInput(inputData){
+        const input = super.getInput(inputData);
+        input.type = 'checkbox';
+        return input;
+    }
+}
+
+class TrueFalseButtons extends RadioWithImages {
+    constructor(question, name, trueVal, falseVal, error){
+        super(question, name, error);
+        this.addInput({
+            id: `${name}yes`,
+            value: trueVal,
+            img: 'img/tick.svg'
+        });
+        this.addInput({
+            id: `${name}no`,
+            value: falseVal,
+            img: 'img/cancel.svg'
+        });
+        this.inputs[0]
+    }
+
+    getLabel(labelData){
+        const label = super.getLabel(labelData);
+        label.classList.add('btn-bool');
+        return label;
     }
 }
