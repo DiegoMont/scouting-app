368181496f8e312eebc7daa33f9d26c51a8d7a2d
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-11 20:07:39 -0600

    Eliminate repeated code

--- js/ScoutingForm.js
+++ js/ScoutingForm.js
@@ -6,6 +6,10 @@ class ScoutingForm {
     constructor(formSelector){
         this.form = document.querySelector(formSelector);
         this.sections = {};
+        this.sections.generalInfo = new ScoutingFormSection('info-match');
+        this.addFormSpecificSections();
+        this.sections.generalInfo.addQuestion(new RegionalSelector());
+        this.sections.comments = new ScoutingFormSection('comments-submit');
     }
 
     renderSections(){
@@ -15,24 +19,25 @@ class ScoutingForm {
             this.form.appendChild(section.container);
         }
     }
+
+    addFormSpecificSections(){}
 }
 
 class MatchScoutingForm extends ScoutingForm {
 
     constructor(form){
         super(form);
-        this.sections.generalInfo = new ScoutingFormSection('info-match');
+    }
+
+    addFormSpecificSections(){
         this.sections.autonomous = new ScoutingFormSection('autonomous-info', 'Autonomous');
         this.sections.teleop = new ScoutingFormSection('teleop-info', 'Driver-Controlled');
-        this.sections.comments = new ScoutingFormSection('comments-submit');
     }
 }
 
 class PitScoutingForm extends ScoutingForm {
     constructor(form){
         super(form);
-        this.sections.generalInfo = new ScoutingFormSection('info-match');
-        this.sections.comments = new ScoutingFormSection('comments-submit');
     }
 }
 
@@ -44,7 +49,7 @@ class ScoutingFormSection {
     constructor(className, title=''){
         this.questions = new Array();
         this.container = document.createElement('div');
-        this.container.classList.add(className, 'centre');
+        this.container.classList.add(className, 'form-centre');
         if(title !== '') {
             const sectionTitle = document.createElement('h1');
             sectionTitle.innerText = title;
