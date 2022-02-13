4d806113776c781daaeeb38e4b982e2e47ec2684
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-13 00:35:47 -0600

    Deleted public directory

--- public/js/settings.js
+++
@@ -1,3 +0,0 @@
-const REGIONALES = [
-    ['monterrey', 'Monterrey']
-];
--- public/js/validar-scouting-match.js
+++
@@ -1,4 +0,0 @@
-const validar = function(){
-    // TODO: Implementar función
-    addError("No se ha implementado la función");
-}
