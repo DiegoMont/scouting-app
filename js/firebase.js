8f7bba733156f0bb4feed05cbdf65e6ab457de10
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-14 00:04:46 -0600

    Set up Firebase

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDm9HAfhA4nSxqCdCvXtwWnVArT-B1FA1M",
+    authDomain: "scouting-app-4010.firebaseapp.com",
+    projectId: "scouting-app-4010",
+    storageBucket: "scouting-app-4010.appspot.com",
+    messagingSenderId: "52181276630",
+    appId: "1:52181276630:web:75694610586411d5bf7677"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
