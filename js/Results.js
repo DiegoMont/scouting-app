2b7ecbce5ea7a9e934fd3c68a086b8e226578e73
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-25 12:28:23 -0600

    Display result tables

--- js/Results.js
+++ js/Results.js
@@ -1,6 +1,8 @@
 let resultsListenerAdded = false;
 const teams = {};
 const teamMatches = {};
+const table = document.querySelector('#results-table tbody');
+const tableHeaders = ['team-number', 'match-number', 'auto-park', 'barcode', 'barriers[]', 'capping', 'driver-ability[]', 'duck-delivery-auto', 'duck-delivery-end', 'level-one', 'level-two', 'level-three', 'storage-freight', 'shared-hub', 'preloaded-freight', 'comments', 'regional'];
 
 const fetchResults = function() {
     if(resultsListenerAdded)
@@ -30,14 +32,36 @@ const addChangeListener = function() {
                 if(!teamMatches.hasOwnProperty(teamNumber))
                     teamMatches[teamNumber] = new Array();
                 teamMatches[teamNumber].push(matchInfo);
+                fillTable(matchInfo);
                 updateResults(teamNumber);
             }
         });
     });
 }
 
+const formatResultTables = function() {
+    const headerRow = getRow(tableHeaders);
+    document.querySelector('#results-table thead').appendChild(headerRow);
+}
+
+const fillTable = function(match) {
+    const values = tableHeaders.map(key => match[key]);
+    const matchRow = getRow(values);
+    table.appendChild(matchRow);
+}
+
 const updateResults = function(teamNumber){
     // TODO: Implement method
     console.log(teams[teamNumber]);
     console.log(teamMatches[teamNumber]);
+}
+
+const getRow = function(list){
+    const tableRow = document.createElement('tr');
+    for (const data of list) {
+        const cell = document.createElement('td');
+        cell.innerText = data;
+        tableRow.appendChild(cell);
+    }
+    return tableRow;
 }
