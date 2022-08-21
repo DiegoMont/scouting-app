17ad5133d883d0e72aa947e321df5e79f5ad331f
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-08-21 14:06:28 -0500

    Form builder initial files

--- js/router/FormEditorRouter.js
+++ js/router/FormEditorRouter.js
@@ -4,5 +4,8 @@ class FormEditorRouter extends Router {
         this.pages.editSeason = {
             htmlContainer: document.getElementById('edit-season')
         };
+        this.pages.scoutingFormBuilder = {
+            htmlContainer: document.getElementById('form-builder')
+        };
     }
 }
