49f8a68a04804597e83f466555aa383e89f0fcbd
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-10-08 22:37:05 -0500

    Added match info questions

--- js/language/SpanishStrings.js
+++ js/language/SpanishStrings.js
@@ -1,8 +1,12 @@
 const SpanishStrings = {}
 
+SpanishStrings.ALLIANCE_QUESTION_LABEL = 'Alianza'
 SpanishStrings.AUTONOMOUS_SECTION_TITLE = 'Autonomous'
+SpanishStrings.BLUE_ALLIANCE_TEXT = 'Azul'
 SpanishStrings.COMMENTS_SECTION_TITLE = 'Comentarios'
 SpanishStrings.ENGINEERING_SECTION_TITLE = 'Ingeniería'
+SpanishStrings.MATCH_NUMBER_QUESTION_LABEL = 'Match'
+SpanishStrings.RED_ALLIANCE_TEXT = 'Roja'
 SpanishStrings.REGIONAL_SELECTOR_TEXT = 'Regional'
 SpanishStrings.TEAM_NUMBER_QUESTION_LABEL = 'Equipo'
 SpanishStrings.TEAM_SECTION_TITLE = 'Finanzas y comunicación'
