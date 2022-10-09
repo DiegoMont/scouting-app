0b383462772c7ac7bc57f5e553635ff5e578af03
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-10-08 22:19:55 -0500

    Initializaed forms with proper questions

--- js/models/scouting-form/MatchScoutingForm.js
+++ js/models/scouting-form/MatchScoutingForm.js
@@ -1,3 +1,7 @@
 class MatchScoutingForm extends ScoutingForm {
-    constructor() {}
+    constructor(events) {
+        super(events)
+        this.sections.autonomous = new ScoutingFormSection(ActiveLanguageStrings.AUTONOMOUS_SECTION_TITLE)
+        this.sections.teleop = new ScoutingFormSection(ActiveLanguageStrings.TELEOP_SECTION_TITLE) 
+    }
 }
--- js/models/scouting-form/PitScoutingForm.js
+++ js/models/scouting-form/PitScoutingForm.js
@@ -1,3 +1,7 @@
 class PitScoutingForm extends ScoutingForm {
-    constructor() {}
+    constructor(events) {
+        super(events)
+        this.sections.engineering = new ScoutingFormSection(ActiveLanguageStrings.ENGINEERING_SECTION_TITLE)
+        this.sections.team = new ScoutingFormSection(ActiveLanguageStrings.TEAM_SECTION_TITLE)
+    }
 }
