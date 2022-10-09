cd109fb1f2eff53e33d7c77e51435a6122b4b904
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-10-08 22:19:09 -0500

    Files to support languages

---
+++ js/language/SpanishStrings.js
@@ -0,0 +1,9 @@
+const SpanishStrings = {}
+
+SpanishStrings.AUTONOMOUS_SECTION_TITLE = 'Autonomous'
+SpanishStrings.COMMENTS_SECTION_TITLE = 'Comentarios'
+SpanishStrings.ENGINEERING_SECTION_TITLE = 'Ingeniería'
+SpanishStrings.REGIONAL_SELECTOR_TEXT = 'Regional'
+SpanishStrings.TEAM_NUMBER_QUESTION_LABEL = 'Equipo'
+SpanishStrings.TEAM_SECTION_TITLE = 'Finanzas y comunicación'
+SpanishStrings.TELEOP_SECTION_TITLE = 'Driver-Controlled'
