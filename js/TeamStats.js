d04f591cbadbbb239b7384ea29250f82d84738bb
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2023-04-04 12:30:53 -0700

    Community transit graph

--- js/TeamStats.js
+++ js/TeamStats.js
@@ -1,5 +1,7 @@
 class TeamStats {
 
+    static FIELD_AREAS = {'A': 0, 'B': 1, 'C': 2};
+
     name;
     teamNumber;
 
@@ -12,6 +14,7 @@ class TeamStats {
         }
         this.name = TEAM_NAMES[this.teamNumber];
         // Add more initialization code here
+        this.setAreaDensity(matches);
 
     }
 
@@ -29,8 +32,48 @@ class TeamStats {
         card.appendChild(tableGrid);
         // Append NumericStats HTML tables to display tables nicely in two columns
         // tableGrid.appendChild(new NumericStats([1, 2, 3]).getHTMLTable('Any title'));
-
+        card.appendChild(this.getCommunityZones());
         // Card HTML code goes above
         return card;
+    }
+
+    setAreaDensity(matches){
+        const totalMatches = matches.length;
+        this.communityEntryDensity = [0, 0, 0];
+        this.communityExitDensity = [0, 0, 0];
+        for (const match of matches){
+            for (const zone of match['enter-community[]'])
+                if(zone !== 'None')
+                    this.communityEntryDensity[TeamStats.FIELD_AREAS[zone]]++;
+            for (const exitZone of match['exit-community[]'])
+                if (exitZone !== 'None')
+                    this.communityExitDensity[TeamStats.FIELD_AREAS[exitZone]]++;
+        }
+        const divisor = Math.max(totalMatches+1, 1);
+        for (let i = 0; i < this.communityEntryDensity.length; i++)
+            this.communityEntryDensity[i] /= divisor;
+        for (let i = 0; i < this.communityExitDensity.length; i++)
+            this.communityExitDensity[i] /= divisor;
+        console.log(this.communityEntryDensity);
+    }
+
+    getCommunityZones(){
+        const field = document.createElement('div');
+        field.classList.add('community-zones');
+        field.innerHTML = `<img src="img/charged-up-community.jpg"></img>
+        <div class="field-density red-a comm-entrance" style="opacity: ${this.communityEntryDensity[0]}"></div>
+        <div class="field-density blue-a comm-entrance" style="opacity: ${this.communityEntryDensity[0]}"></div>
+        <div class="field-density red-b comm-entrance" style="opacity: ${this.communityEntryDensity[1]}"></div>
+        <div class="field-density blue-b comm-entrance" style="opacity: ${this.communityEntryDensity[1]}"></div>
+        <div class="field-density red-c comm-entrance" style="opacity: ${this.communityEntryDensity[2]}"></div>
+        <div class="field-density blue-c comm-entrance" style="opacity: ${this.communityEntryDensity[2]}"></div>
+        <div class="field-density red-a comm-exit" style="opacity: ${this.communityExitDensity[0]}"></div>
+        <div class="field-density blue-a comm-exit" style="opacity: ${this.communityExitDensity[0]}"></div>
+        <div class="field-density red-b comm-exit" style="opacity: ${this.communityExitDensity[1]}"></div>
+        <div class="field-density blue-b comm-exit" style="opacity: ${this.communityExitDensity[1]}"></div>
+        <div class="field-density red-c comm-exit" style="opacity: ${this.communityExitDensity[2]}"></div>
+        <div class="field-density blue-c comm-exit" style="opacity: ${this.communityExitDensity[2]}"></div>
+        `;
+        return field;
     }
 }
