80b331e157883faa8408c6ffbe57f85480f0990f
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2023-04-04 16:29:02 -0700

    Word limit corrections

--- js/match-form.js
+++ js/match-form.js
@@ -5,13 +5,13 @@ function createMatchForm() {
 
     const robotStrategy = new BigTextArea("What is the overall game strategy of the robot?", "strategy", MIN_WRITTEN_WORDS);
     robotStrategy.wordLimitExceededMsg = "The description is too big. Summarize it more!";
-    robotStrategy.minWordsLimit = MAX_WRITTEN_WORDS;
+    robotStrategy.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const robotStrenghts = new BigTextArea("What are the strenghts of the robot and/or game strategy?", "robot-strengths", MIN_WRITTEN_WORDS);
-    robotStrenghts.minWordsLimit = MAX_WRITTEN_WORDS;
+    robotStrenghts.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const improvementArea = new BigTextArea("What is the improvement area of the robot and/or game strategy?", "strategy-improvement", MIN_WRITTEN_WORDS);
-    improvementArea.minWordsLimit = MAX_WRITTEN_WORDS;
+    improvementArea.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const technicalProblems = new CheckboxWithText("Which problems they suffered during the match?", "technical-problems");
     technicalProblems.addInput({
@@ -32,7 +32,7 @@ function createMatchForm() {
     })
 
     const autoStrategy = new BigTextArea("What the robot does in the autonomous period?", "autonomous-strategy", MIN_WRITTEN_WORDS);
-    autoStrategy.minWordsLimit = MAX_WRITTEN_WORDS;
+    autoStrategy.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const autoTasks = new TrueFalseButtons("Was it successful in completing the tasks it was supposed to perform?", "auto-success", "Yes", "No", "Indicate if the robot succeded accomplishing its autonomous tasks");
 
@@ -55,7 +55,7 @@ function createMatchForm() {
     });
 
     const teleopStrategy = new BigTextArea("What is the strategy of the robot in the TeleOp period?", "teleop-strategy", MIN_WRITTEN_WORDS);
-    teleopStrategy.minWordsLimit = MAX_WRITTEN_WORDS;
+    teleopStrategy.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const gamePieces = new CheckboxWithText("Where does the robot accommodate each game piece?", "game-pieces-location");
     gamePieces.addInput({
@@ -92,7 +92,7 @@ function createMatchForm() {
     const exitCommunity = new FieldZones("Which area they use to exit from the Community zone?", 'exit-community', ['A', 'B', 'C', 'None'], '');
 
     const endgameStrategy = new BigTextArea("What is the robot strategy during the endgame period?", "endgame-strategy", MIN_WRITTEN_WORDS);
-    endgameStrategy.minWordsLimit = MAX_WRITTEN_WORDS;
+    endgameStrategy.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const endgamePark = new RadioWithText("Where was the robot parked at the end of the match?", "endgame-park");
     endgamePark.addInput({
--- js/pit-form.js
+++ js/pit-form.js
@@ -50,12 +50,12 @@ function createPitForm() {
     });
 
     const matchFocus = new BigTextArea('Which task does the robot do best during a match?', 'match-focus', MIN_WRITTEN_WORDS);
-    matchFocus.minWordsLimit = MAX_WRITTEN_WORDS;
+    matchFocus.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const vision = new TrueFalseButtons('The robot uses vision to align with the nodes?', 'Vision', 'Uses vision', 'Does not use vision');
 
     const allyNeeds = new BigTextArea('What do they expect from other robots in an alliance?', 'ally-needs', MIN_WRITTEN_WORDS);
-    allyNeeds.minWordsLimit = MAX_WRITTEN_WORDS;
+    allyNeeds.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const chargingStationBalance = new TextInput('Which sensors do they use to balance the robot on the Charge Station?', 'charge-station-sensors', 'NavX, accelerometer, IMU', 1);
 
@@ -70,25 +70,25 @@ function createPitForm() {
     pitForm.sections.engineering.addQuestion(robotPic);
 
     const sponsors = new BigTextArea("List the team's sponsors and strategic alliances", 'Sponsors', MIN_WRITTEN_WORDS);
-    sponsors.minWordsLimit = MAX_WRITTEN_WORDS;
+    sponsors.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const representativeSponsors = new BigTextArea('Which sponsors or strategic alliances best represents and/or defines their team?', 'representative-sponsors', MIN_WRITTEN_WORDS);
-    representativeSponsors.minWordsLimit = MAX_WRITTEN_WORDS;
+    representativeSponsors.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const funding = new BigTextArea('How do they fund their team?', 'Funding', MIN_WRITTEN_WORDS);
-    funding.minWordsLimit = MAX_WRITTEN_WORDS;
+    funding.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const projects = new BigTextArea('Which are the most relevant team projects?', 'Projects', MIN_WRITTEN_WORDS);
-    projects.minWordsLimit = MAX_WRITTEN_WORDS;
+    projects.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const characteristics = new BigTextArea('What are the characteristics or elements that differentiate their team from the others?', 'Identity', MIN_WRITTEN_WORDS);
-    characteristics.minWordsLimit = MAX_WRITTEN_WORDS;
+    characteristics.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const socialNetworks = new BigTextArea('Social media accounts', 'Accounts', MIN_WRITTEN_WORDS);
-    socialNetworks.minWordsLimit = MAX_WRITTEN_WORDS;
+    socialNetworks.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     const mediaStrategy = new BigTextArea('What is their social media strategy?', 'social-media', MIN_WRITTEN_WORDS);
-    mediaStrategy.minWordsLimit = MAX_WRITTEN_WORDS;
+    mediaStrategy.maxWordsLimit = MAX_WRITTEN_WORDS;
 
     pitForm.sections.team.addQuestion(funding);
     pitForm.sections.team.addQuestion(sponsors);
