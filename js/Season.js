6b34c0f05d6e0bd864d97f09e94f6b0c394206e3
Diego Montaño <31603020+DiegoMont@users.noreply.github.com>
2024-03-23 10:27:37 -0600

    Colorado regional fix

--- js/Season.js
+++ js/Season.js
@@ -1,8 +1,9 @@
 class Season {
-    static SEASON_NAME = '';
+    static SEASON_NAME = 'crescendo';
     static REGIONALES = [
-        'Monterrey'
+        'Colorado'
     ];
 }
 
 language = new SpanishText();
+const storageDir = Season.SEASON_NAME + Season.REGIONALES[0].toLowerCase().replace(' ', '-');
